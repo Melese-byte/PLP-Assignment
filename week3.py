38ae6cf9711dda2d9d6643c661ae863eafd5311b
Melese <melesealemante02@gmail.com>
2025-08-02 13:17:12 -0700

    Week 3 Assignments

---
+++ week3.py
@@ -0,0 +1,9 @@
+"""Create a function named calculate_discount(price, discount_percent) that calculates the final price after applying a discount. 
+The function should take the original price (price) and the discount percentage (discount_percent) as parameters. 
+If the discount is 20% or higher, apply the discount; otherwise, return the original price."""
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        final_price = price * (1 - discount_percent / 100)
+        return final_price
+    else:
+        return price
